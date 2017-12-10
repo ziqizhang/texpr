3a0b5d632879581b6d8e951e96bf8af9ae49622c
Johann Petrak <johann.petrak@gmail.com>
2017-12-10 12:04:52 +0000

    Implement better approach: filtering and using string similarity already in here, better output, include top l% and require a certain minimum embedding similarity.

--- code/embeddings/create-mostSim4Onto.sh
+++ code/embeddings/create-mostSim4Onto.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-if [[ "x${EMBEDDINGS_DIR}" == "x" ]]
-then
-  echo 'Environment variable EMBEDDINGS_DIR should be set to where the embeddings file is stored'
-  exit 1
-fi
+## create the mostSim4Onto files for all 4 embeddings
 
-# use python3!
-python mostSim4Onto.py ../../data/knowmak-ontology.lst corpuswords.tsv ${EMBEDDINGS_DIR}/glove.840B.300d.gensim > mostSim4Onto.tsv
+python3 mostSim4Onto.py ../../data/knowmak-ontology.lst corpuswords-facebook.tsv ../../embeddings/wiki.en.gensim 0 > mostSim4Onto-facebook.tsv
+
+python3 mostSim4Onto.py ../../data/knowmak-ontology.lst corpuswords-glove.6B.tsv ../../embeddings/glove.6B.300d.gensim 0 > mostSim4Onto-glove.6B.tsv
+
+python3 mostSim4Onto.py ../../data/knowmak-ontology.lst corpuswords-glove.840B.tsv ../../embeddings/glove.840B.300d.gensim 1 > mostSim4Onto-glove.840B.tsv
+
+python3 mostSim4Onto.py ../../data/knowmak-ontology.lst corpuswords-googlenews.tsv ../../embeddings/GoogleNews-vectors-negative300.gensim 1 > mostSim4Onto-googlenews.tsv
--- code/embeddings/mostSim4Onto.py
+++ code/embeddings/mostSim4Onto.py
@@ -10,6 +10,13 @@
 ## This means the program will output as many lines as there are words in the second
 ## file: for each corpus word, the keyword which matches best and its similarity
 
+## output file format is tsv with the following columns:
+## * score name, one of simonly, simidfonly, simidfstr
+## * corpus word
+## * keyword
+## * rank number (0 is highest, 1 is next etc.)
+## * score value
+
 ## Author: Johann Petrak <johann.petrak@gmail.com>
 
 
@@ -18,56 +25,124 @@ import sys
 import os
 from embeddingsutils import EmbeddingsUtils
 from tqdm import tqdm
+import heapq
+import numpy as np
+from difflib import SequenceMatcher
 
-if len(sys.argv) != 4:
-    print("ERROR: need the following arguments: keywords lst file, corpus words tsv file, embeddings path",file=sys.stderr)
+if len(sys.argv) != 5:
+    print("ERROR: need the following arguments: keywords lst file, corpus words tsv file, embeddings path, 0/1 (0=lowercase/1=mixed case embeddings)",file=sys.stderr)
     sys.exit(1)
 
 keywordsFile = sys.argv[1]
 corpusWordsFile = sys.argv[2]
 embFile = sys.argv[3]
-
+mixedCase = (sys.argv[4]=="1")
+print("INFO: using mixed case settings for the embeddings!",file=sys.stderr)
 eu = EmbeddingsUtils()
-eu.setIsCaseSensitive(False)
-eu.setFallBackToLower(False)
+eu.setIsCaseSensitive(mixedCase)
+eu.setFallBackToLower(mixedCase)
 eu.setFilterStopwords(True)
 eu.setDebug(False)
-eu.setVerbose(False)
+eu.setVerbose(True)
 eu.loadEmbeddings(embFile)
 
+MIN_EMB_SIM=0.5      # ignore anything where the embedding similarity is less
+KEEP_FRACTION=0.2    # keep that portion of highest scoring words, after filtering
+
 keywords = set()
-print("Reading keywords ...",file=sys.stderr)
+print("Reading ontology keywords ...",file=sys.stderr)
 with open(keywordsFile) as inp:
     for line in inp:
         (keyword,cname,other) = line.split("\t")
         ## cname = cname[4:]
         keywords.add(keyword)
 
+print("INFO: got keywords:",len(keywords),file=sys.stderr)
 corpuswords = {}
-print("Reading keywords and their idf ...",file=sys.stderr)
+print("Reading corpus words and their idf ...",file=sys.stderr)
 with open(corpusWordsFile) as inp:
     for line in inp:
         line = line.strip()
         (word,idf) = line.split("\t")
         corpuswords[word] = float(idf)
+print("INFO: got corpus words:",len(corpuswords),file=sys.stderr)
 
-print("Calculating similarities...",file=sys.stderr)
+print("Calculating similarities, this will take a while ...",file=sys.stderr)
 
-with tqdm(total=len(corpuswords)) as pbar:
-    for word,idf in corpuswords.items():
-        bestkeyword = None
-        bestsim = -1.0
-        bestkeyword2 = None
-        bestsim2 = -1.0
-        for keyword in keywords:
-            (sim,used1,used2) = eu.sim4texts(keyword,word)
-            if sim > bestsim:
-                bestkeyword = keyword
-                bestsim = sim
-            sim2 = sim * idf
-            if sim2 > bestsim2:
-                bestkeyword2 = keyword
-                bestsim2 = sim2
-        print("sim",word,bestkeyword,bestsim,sep="\t")
-        print("sim*idf",word,bestkeyword2,bestsim2,sep="\t")
-        pbar.update(1)
+## Implementation note:
+## * corpus words are filtered/ignored if the corpus word is identical or a word in a key phrase
+## * embedding similarity is calculated, corpus word is ignored if sim < MIN_EMB_SIM
+## * the following scores are calculated for the pair:
+##   * raw embedding sim (sim)
+##   * embedding sim times corpus idf (simidf)
+##   * embedding sim times corpus idf times inverse string sim (simidfstr)
+## * three ordered lists for each of the scores are maintained 
+## * the highest k corpus words for each of the three scores are output, together
+##   with their rank numbers
+
+# k is the number of corpus words that corresponds to KEEP_FRACTION
+k = int(len(corpuswords)*KEEP_FRACTION)
+totalpairs = 0
+written = 0
+with tqdm(total=(len(corpuswords)*len(keywords))) as pbar:
+    for keyword in keywords:
+        h_sim = []
+        h_simidf = []
+        h_simidfstr = []
+        for corpusword,idf in corpuswords.items():
+            pbar.update(1)
+            totalpairs = totalpairs + 1
+            # filter words we are not interested in
+            # 1) the corpus word is equal to the keyword according to the embeddings
+            (embsim,usedcorpuswords,usedkeywords) = eu.sim4texts(corpusword,keyword)
+            # print("DEBUG embsim for",corpusword,"/",keyword,"=",embsim,file=sys.stderr)
+            if np.isclose(embsim, 1.0, rtol=1e-07, atol=1e-09, equal_nan=False):
+                continue
+            # 2) the minimum embedding similarity threshold is not reached
+            if embsim < MIN_EMB_SIM:
+                continue
+            # 3) one of the (used) corpuswords is in the keyword words
+            isContained = False
+            for w in usedcorpuswords:
+               if w in usedkeywords:
+                   isContained = True
+                   break
+            if isContained:
+                # print("DEBUG: skipping, isContained",file=sys.stderr)
+                continue
+            # ok, we want to keep this pair, store the corpusword in the heaps, for their scores
+            # print("DEBUG: pushing ",(embsim,corpusword),file=sys.stderr)
+            heapq.heappush(h_sim,(embsim,corpusword))
+            simidf = embsim * idf
+            heapq.heappush(h_simidf,(simidf,corpusword))
+            stringsim = SequenceMatcher(None,corpusword,keyword).ratio()
+            simidfstr = embsim * idf * (1.0/(stringsim+1))
+            heapq.heappush(h_simidfstr,(simidfstr,corpusword))
+        # after going through all the corpuswords, output the best k 
+        if len(h_sim) == 0:
+            # print("DEBUG: h_sim is empty",file=sys.stderr)
+            pass
+        else:
+            # print("DEBUG: heap is ",h_sim,file=sys.stderr)
+            l1 = heapq.nlargest(k,h_sim)
+            rank = 0
+            for s,w in l1:
+               print("simonly",w,keyword,rank,s,sep="\t")
+               written = written + 1
+               rank = rank + 1
+            l1 = heapq.nlargest(k,h_simidf)
+            rank = 0
+            for s,w in l1:
+               print("simidfonly",w,keyword,rank,s,sep="\t")
+               written = written + 1
+               rank = rank + 1
+            l1 = heapq.nlargest(k,h_simidfstr)
+            rank = 0
+            for s,w in l1:
+               print("simidfstr",w,keyword,rank,s,sep="\t")
+               written = written + 1
+               rank = rank + 1
+    
+print("Finished, total comparisons:",totalpairs,"written:",written,file=sys.stderr)
+
+
