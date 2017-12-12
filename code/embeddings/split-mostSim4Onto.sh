8458b4a613cab718e5729a48e1b015313b4da4a1
Johann Petrak <johann.petrak@gmail.com>
2017-12-12 10:36:05 +0000

    Filter by max rank when splitting.

--- code/embeddings/split-mostSim4Onto.sh
+++ code/embeddings/split-mostSim4Onto.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 infile="$1"
-if [[ "x$infile" == "x" ]]
+maxrank="$2"
+if [[ "x$maxrank" == "x" ]]
 then 
-  echo need one parameter, the name of the input file
+  echo need two parameters, the name of the input file and max rank
   exit 1
 fi
 
 inpref=`basename $infile .tsv`
-grep "simonly" $infile | cut -f 2- | sort -t$'\t' --key=4nr,4  > ${inpref}-sim.tsv
-grep "simidfonly" $infile | cut -f 2- | sort -t$'\t' --key=4nr,4  > ${inpref}-simidf.tsv
-grep "simidfstr" $infile | cut -f 2- | sort -t$'\t' --key=4nr,4  > ${inpref}-simidfstr.tsv
+grep "simonly" $infile | perl -F'\t' -ane "if (\$F[3] < $maxrank) {print}" | cut -f 2- | sort -t$'\t' --key=4nr,4  > ${inpref}-sim.tsv
+grep "simidfonly" $infile | perl -F'\t' -ane "if (\$F[3] < $maxrank) {print}" | cut -f 2- | sort -t$'\t' --key=4nr,4  > ${inpref}-simidf.tsv
+grep "simidfstr" $infile | perl -F'\t' -ane "if (\$F[3] < $maxrank) {print}" | cut -f 2- | sort -t$'\t' --key=4nr,4  > ${inpref}-simidfstr.tsv
 
