d86208b71e2c4bd054182d02e8f0c7c5a2822416
Johann Petrak <johann.petrak@gmail.com>
2018-01-08 18:54:27 +0000

    Beginning of script for creating the final rankings.

---
+++ code/embeddings/finalRanking.py
@@ -0,0 +1,58 @@
+## Script for creating the final ranking file from the following inputs:
+## - the mostSim4Onto-EMB.tsv file (with all the similarities and full ranks)
+## - the corpusword-textrank tsv file which maps corpus words to textrank
+## - the keyword-class uri tsv file which maps keywords to class URIs and sections
+
+## the script writes a tsv file which contains the following information:
+## - the class URL
+## - the section
+## - the keyword
+## - the corpus word
+## - a similarity score name
+## - a score
+## - a rank
+## Author: Johann Petrak <johann.petrak@gmail.com>
+
+
+from __future__ import print_function
+import sys
+import os
+from embeddingsutils import EmbeddingsUtils
+from tqdm import tqdm
+import heapq
+import numpy as np
+from difflib import SequenceMatcher
+from collections import OrderedDict
+
+if len(sys.argv) != 5:
+    print("ERROR: need the following arguments: mostSim-file, textrank-file, classinfo-file, output-file",file=sys.stderr)
+    sys.exit(1)
+
+debug=False
+verbose=True
+mixedCase=True
+
+mostSimFile = sys.argv[1]
+textRankFile = sys.argv[2]
+classInfoFile = sys.argv[3]
+outputFile = sys.argv[4]
+
+## first read in all the corpus word to text rank mappings and remember them
+textRank = {}
+n_textranks = 0
+with open(textRankFile) as infile:
+    for line in infile:
+        n_textranks += 1
+        line = line.strip()
+        (word,tr) = line.split("\t")
+        textRank[word] = tr
+
+## read in all the keyword to class uri and to section mappings and remember
+keyword2info = {}
+n_kwinfos = 0
+with open(classInfoFile) as infile:
+    for line in infile:
+        n_kwinfos += 1
+        line = line.strip()
+        (kw,uri,sec) = line.split("\t")
+        keyword2info[kw] = (uri,sec)
