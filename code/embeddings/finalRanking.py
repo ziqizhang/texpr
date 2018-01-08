cf3e1d3758c99bc9f05a00a4932c821ffc84a8d2
Johann Petrak <johann.petrak@gmail.com>
2018-01-08 20:02:06 +0000

    A bit more.

--- code/embeddings/finalRanking.py
+++ code/embeddings/finalRanking.py
@@ -56,3 +56,37 @@ with open(classInfoFile) as infile:
         line = line.strip()
         (kw,uri,sec) = line.split("\t")
         keyword2info[kw] = (uri,sec)
+
+
+## Read in the rows for one keyword-corpusword pair and similarity measure,
+## calculate all the additional similarities and then put into a sorting
+## queue. Output original similarity plus rank and then the derived
+## similarity(ies) plus rank
+
+## The columns of the input file should be:
+## 1 - name of similarity score: simonly, simidfonly, simidfstr
+## 2 - corpus word
+## 3 - key word
+## 4 - rank according to the similarity score
+## 5 - similarity score
+## 6 - corpus word as used
+## 7 - key words as used
+## 8 - idf of corpus word
+## 9 - average idf of key words used
+## 10 - similarity times average key word idf (does not influence ranking since same keyword)
+with open(outputFile,"w") as outfile:
+    with open(mostSimFile) as infile:
+        oldkey = ""   ## key is sim+keyword
+        for line in infile:
+            line = line.strip()
+            (simname,cword,kword,rank,score,unused1,unused2,idfc,idfk,simidfk) = line.split("\t")
+            key = simname+"|"+kword
+            if key == oldkey:
+                pass
+            else:
+                if oldkey:
+                    ## finish and output the previous list
+                oldkey = key
+                # start a new queue
+    # finished the whole input file, but we still have to finish the last key
+    # TODO!!
